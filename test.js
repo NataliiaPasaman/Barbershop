64c2bdfbae0f4a7ae9b1fd46c6eb03df1c8ed76e
NataliiaPasaman <natapasaman@gmail.com>
2022-07-06 00:38:26 +0300

    js для тестів

---
+++ test.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const x = 10;
+
+let y = 25;
+
+y = 6;
+
+let age;
+console.log(age);
+
+
+const username = "Viks";
+console.log(username);
+console.log(username.toUpperCase());
+
+
+const file = "script.js";
+console.log(file.includes());
+
+
+// const productPrice1 = 250;
+// const productPrice2 = 150;
+// const productPrice3 = 350;
+// alert (productPrice1);
+// alert (productPrice2);
+// alert (productPrice3);
+
+const isRead = confirm('Do you want read this site?');
+console.log(isRead);
+
+const isQuantity = prompt('Please, input number of product');
+console.log(isQuantity);
